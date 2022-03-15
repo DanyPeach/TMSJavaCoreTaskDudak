b98039cce80d1406fe4a10233b226331c1eab87e
Daniil Dudak <daniildudak2002@gmail.com>
2022-03-16 00:09:39 +0300

    task 6 complete

---
+++ src/by/TMS_Dudak/Lesson3/Task6.java
@@ -0,0 +1,10 @@
+package by.TMS_Dudak.Lesson3;
+
+public class Task6 {
+    public static void main(String[] args) {
+        double a = 1.2;
+        double b = 1.21;
+
+        System.out.println("Число являющееся большим из списка( "+ a +" , "+ b +") является: "+ Math.max(a, b));
+    }
+}

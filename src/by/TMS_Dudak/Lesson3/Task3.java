986d8cb48f700a4333db25a68f46386567e09b4f
Daniil Dudak <daniildudak2002@gmail.com>
2022-03-15 22:43:53 +0300

    task 3 complete

---
+++ src/by/TMS_Dudak/Lesson3/Task3.java
@@ -0,0 +1,20 @@
+package by.TMS_Dudak.Lesson3;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int a = 12;
+
+        if(a>0){
+            a++;
+            System.out.println(a);
+        }else if(a<0){
+            a--;
+            a--;
+            System.out.println(a);
+        }else{
+            a = 10;
+            System.out.println(a);
+        }
+    }
+
+}

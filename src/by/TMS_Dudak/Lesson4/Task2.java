6063f05ef2845162c4b4da6e516d470be2f14e5a
Daniil Dudak <daniildudak2002@gmail.com>
2022-03-16 14:31:58 +0300

    add Task2

---
+++ src/by/TMS_Dudak/Lesson4/Task2.java
@@ -0,0 +1,15 @@
+package by.TMS_Dudak.Lesson4;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int amoeba = 1;
+        int hours = 0;
+
+        for(int i = amoeba; hours < 25; i*=2){
+            System.out.println("За следующее количесвто часов:  "+ hours + " , амеб стало " + i);
+            hours++;
+            hours++;
+            hours++;
+        }
+    }
+}

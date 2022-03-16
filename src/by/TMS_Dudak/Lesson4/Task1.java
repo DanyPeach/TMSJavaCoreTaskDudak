faad2e9e601f8c9cf9b10cffa7e5d5245eb9c169
Daniil Dudak <daniildudak2002@gmail.com>
2022-03-16 14:21:57 +0300

    add new packege +  Task1

---
+++ src/by/TMS_Dudak/Lesson4/Task1.java
@@ -0,0 +1,15 @@
+package by.TMS_Dudak.Lesson4;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int distance = 10;
+        int day = 1;
+
+        while(day<7){
+            distance+=0.1*distance;
+            day++;
+        }
+
+        System.out.println("За " + day + " дней спортсмен пробежал " + distance + " километров");
+    }
+}

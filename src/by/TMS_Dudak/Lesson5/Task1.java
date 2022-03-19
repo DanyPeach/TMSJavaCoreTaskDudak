318b30d24f93d4e544929b2cca9d816925a895e9
Daniil Dudak <daniildudak2002@gmail.com>
2022-03-19 15:34:19 +0300

    add l5 package + task1

---
+++ src/by/TMS_Dudak/Lesson5/Task1.java
@@ -0,0 +1,20 @@
+package by.TMS_Dudak.Lesson5;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] a = {2,4,6,8,10,12,14,16,18,20};
+
+
+        for(int i = 0; i<10; i++){
+            System.out.print(a[i] + " ");
+            if(i==9){
+                System.out.println("\n");
+            }
+
+        }
+
+        for(int i = 0; i<10; i++){
+            System.out.println(a[i]);
+        }
+    }
+}

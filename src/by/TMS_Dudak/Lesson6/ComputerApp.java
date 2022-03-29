9abff80cde326bbb58ad42cd1144eeec0db6ce39
Daniil Dudak <daniildudak2002@gmail.com>
2022-03-29 19:01:57 +0300

    Взрыв компбютера(

---
+++ src/by/TMS_Dudak/Lesson6/ComputerApp.java
@@ -0,0 +1,10 @@
+package by.TMS_Dudak.Lesson6;
+
+public class ComputerApp {
+    public static void main(String[] args) {
+        Computer Laptop = new Computer("LFG-6098", "Windows 10", "Getforce gtx");
+        Laptop.displayInfo();
+        Laptop.turnOn();
+
+    }
+}
